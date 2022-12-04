5ff90adba41d35f8ed15b7352973d1240334db17
Kevin Zhang <kzhang1@imsa.edu>
2022-12-04 16:40:33 -0600

    Trying to pull rn

--- slingshot.py
+++ slingshot.py
@@ -1,9 +1,6 @@
 from graphics import *
-<<<<<<< HEAD
 
 def drawSlingshot(bx, by, win):
     slingshot = Polygon(Point(bx - 15, by - 5), Point(bx - 10, by - 10), Point(bx, by + 10), Point(bx + 15, by - 5), Point(bx + 20, by), Point(bx, by + 50))
     slingshot.setFill('brown')
     slingshot.draw(win)
-=======
->>>>>>> 052943862a1b9c28d7d96da3294d7ba8de6634a8
