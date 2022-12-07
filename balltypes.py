b3b82fb9ada91ba460a13fc5a3272f0321613e92
nekiwo <58634064+nekiwo@users.noreply.github.com>
2022-12-07 15:24:29 -0600

    quick formatting

--- balltypes.py
+++ balltypes.py
@@ -4,7 +4,7 @@ from math import *
 class Ball:
 
     def __init__(self, params, win):
-        # unpacks the dictonary input and sets all fields equal to their initial values
+        # Unpacks the dictionary input and sets all fields equal to their initial values
         self.acc = params["acc"]
         self.vel = params["vel"]
         self.pos = params["pos"]
@@ -18,13 +18,13 @@ class Ball:
         self.circle.setFill(self.color)
         self.circle.draw(self.win)
 
-    def move(self, x, y): # the Ball move function uses the circle move function from Graphics.py
+    def move(self, x, y): # The Ball move function uses the circle move function from Graphics.py
         self.circle.move(x, y)
 
     def click(self):
         if (self.canClick):
-            self.canClick = False # so you can only click once
-            v2 = self.vel[0]**2 + self.vel[1]**2
+            self.canClick = False # So you can only click once
+            v2 = self.vel[0] ** 2 + self.vel[1] ** 2
 
             if (self.color == "yellow"):
                 if (v2 == 0):
@@ -33,7 +33,7 @@ class Ball:
                     self.vel[0] += 5*(self.vel[0]/sqrt(v2))
                     self.vel[1] += 5*(self.vel[1]/sqrt(v2))
                 
-            if (self.color == "blue"): # creates two new balls, one above one below, and returns them to be added to the balls array
+            if (self.color == "blue"): # Creates two new balls, one above one below, and returns them to be added to the balls array
                 return [Ball({"acc" : self.acc, "vel" : [self.vel[0] + 1*self.vel[1]/sqrt(v2), self.vel[1] - 1*self.vel[0]/sqrt(v2)], "pos" : self.pos, "prevPos" : self.pos, "color" : "blue", "radius": 6}, self.win),
                         Ball({"acc" : self.acc, "vel" : [self.vel[0] - 1*self.vel[1]/sqrt(v2), self.vel[1] + 1*self.vel[0]/sqrt(v2)], "pos" : self.pos, "prevPos" : self.pos, "color" : "blue", "radius": 6}, self.win)]
     
@@ -49,21 +49,20 @@ class Ball:
 
 class Block():
     def __init__(self, params, win):
-        # unpacks the dictonary input and sets all fields equal to their initial values
+        # Unpacks the dictionary input and sets all fields equal to their initial values
         self.acc = params["acc"]
         self.vel = params["vel"]
         self.pos = params["pos"]
         self.pos2 = params["pos2"]
         self.prevPos = params["prevPos"]
         self.color = params["color"]
-        # self.radius = self.pos2[0] - self.pos[0]
         self.win = win
 
         self.rect = Rectangle(Point(self.pos[0], self.pos[1]), Point(self.pos2[0], self.pos2[1]))
         self.rect.setFill(self.color)
         self.rect.draw(self.win)
 
-    def move(self, x, y): # the Block move function uses the rect move function from Graphics.py
+    def move(self, x, y): # The Block move function uses the rect move function from Graphics.py
         self.rect.move(x, y)
 
     def undraw(self):
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def updates(ball, colliders, win):
                 p1 = collider[0]
                 p2 = collider[1]
             except:
-                t = 'b'
+                t = "b"
                 p1 = collider.pos
                 p2 = collider.pos2
             # Subtract coords so that the ball is at (0, 0)
@@ -49,10 +49,10 @@ def updates(ball, colliders, win):
                     ball.acc = [0, 0] # Remove gravity when collided
                 ball.vel = [ball.vel[0] + ball.vel[0] * normal[0], ball.vel[1] + ball.vel[1] * normal[1]]
                 ball.pos = [ball.pos[0] + ball.vel[0], ball.pos[1] + ball.vel[1]]
-                if t == 'b':
+                if t == "b":
                     print(ball.pos)
                 
-                if(t == 'b'):
+                if(t == "b"):
                     for item in win.items[:]:
                         if item.__class__.__name__ == "Line":
                             if item.p1.x == collider[0][0] and item.p2.y == collider[1][1]:
@@ -78,7 +78,7 @@ def updates(ball, colliders, win):
         ball.prevPos = ball.pos
 
 def main():
-    win = GraphWin("Title", 1000, 500, autoflush=False)
+    win = GraphWin("Irritated birds", 1000, 500, autoflush=False)
 
     #Draws background
     grass = Rectangle(Point(0,400),Point(win.width,500))
@@ -93,32 +93,22 @@ def main():
     # 0: point #1
     # 1: point #2
     # 2: normal force vector
+    # 3: type ("f" for floor, "b" for block)
     colliders = [
-        [[0, 400], [win.width, 400], [friction, -1.5], 'f'], # Ground
-        #[[0.000000001, -500], [0.000000001, 500], [-1.2, 0], 'f'], # Left Border
-        #[[win.width - 0.000000001, -500], [win.width - 0.000000001, 500], [-1.2, 0], 'f'], # Right Border
-        #[[500, 200], [700, 200], [0, -1.6], 'b'],
-        #[[700, 400], [700, 200], [-1.4, 0], 'b'],
-        #[[500, 200], [400, 100], [-1.8*cos(pi/4), -1.8*sin(pi/4)], 'b'],
-        #[[400, 100], [400, 0], [-1.4, 0], 'b']
+        [[0, 400], [win.width, 400], [friction, -1.5], "f"], # Ground
     ]
 
     # Render colliders
     for collider in colliders:
         lineP1 = collider[0]
         lineP2 = collider[1]
-        lineN = collider[2]
-        # Render collider line
-        p1 = Point(lineP1[0], lineP1[1])
-        p2 = Point(lineP2[0], lineP2[1])
-        line = Line(p1, p2)
-        if(collider[3] == 'b'):
-            line.setOutline(color_rgb(153, 51, 0))
-            line.setWidth(5);
+        point1 = Point(lineP1[0], lineP1[1])
+        point2 = Point(lineP2[0], lineP2[1])
+        line = Line(point1, point2)
         line.draw(win)
     
     
-    # adds the blocks at the end that don't have gravity, but disappear when hit
+    # Adds the blocks at the end that don"t have gravity, but disappear when hit
     blocks.append(Block({"acc" : [0, 5.4], "vel" : [0, 0], "pos" : [420, 320], "pos2" : [430, 400], "prevPos" : [300, 200], "color" : "brown"}, win))
     blocks.append(Block({"acc" : [0, 5.4], "vel" : [0, 0], "pos" : [510, 320], "pos2" : [520, 400], "prevPos" : [300, 200], "color" : "brown"}, win))
     blocks.append(Block({"acc" : [0, 5.4], "vel" : [0, 0], "pos" : [520, 320], "pos2" : [420, 310], "prevPos" : [300, 200], "color" : "brown"}, win))
@@ -141,10 +131,12 @@ def main():
 
     # Ball physics variables
     ballPos = [100, 360]
-    ballVars = [{"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "red", "radius": 10}, 
-    {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "yellow", "radius": 10}, 
-    {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "blue", "radius": 6},
-    {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : color_rgb(192,192,192), "radius": 12}]
+    ballVars = [
+        {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "red", "radius": 10}, 
+        {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "yellow", "radius": 10}, 
+        {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "blue", "radius": 6},
+        {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : color_rgb(192,192,192), "radius": 12}
+    ]
     drawSlingshot(ballPos[0], ballPos[1], win)
 
     # Creates ball and sets its velocity according to the slingshot
@@ -158,9 +150,6 @@ def main():
     while win.isOpen():
         for ball in balls: # updates every ball in the system
             updates(ball, colliders, win)
-
-        #for block in blocks:
-        #    updates(block, blocks, win)
 
         # Press ESC to close window
         if win.checkKey() == "Escape":
@@ -181,8 +170,8 @@ def main():
                 if len(balls) == 0:
                     ballVars = [{"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "red", "radius": 10}, {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "yellow", "radius": 10}, {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : "blue", "radius": 6}, {"acc" : [0, 5.88/fps], "vel" : [1, 0], "pos" : ballPos, "prevPos" : ballPos, "color" : color_rgb(192,192,192), "radius": 10}]
                     ball = Ball(ballVars[randint(0, 3)], win)
-                    velcoords = slingMouse(win)
-                    ball.vel = [0.1*-(velcoords[0] - ballPos[0]), 0.1*-(velcoords[1] - ballPos[1])]
+                    velCoords = slingMouse(win)
+                    ball.vel = [0.1*-(velCoords[0] - ballPos[0]), 0.1*-(velCoords[1] - ballPos[1])]
                     balls.append(ball)
                     break
 
@@ -192,12 +181,12 @@ def main():
         if len(blocks) == 0:
             for b in balls:
                 b.removeBall()
-            txt = Text(Point(win.width/2, win.height/2), 'You Win!')
+            txt = Text(Point(win.width / 2, win.height / 2), "You Win!")
             txt.setSize(35)
-            txt.setFill('black')
+            txt.setFill("black")
             txt.draw(win)
-            ex = Text(Point(win.width/2, 9*win.height/10), 'Click to close window')
-            ex.setFill('black')
+            ex = Text(Point(win.width / 2, 9 * win.height / 10), "Click to close window")
+            ex.setFill("black")
             ex.draw(win)
             win.getMouse()
             win.close()
